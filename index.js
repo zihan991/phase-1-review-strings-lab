43a2c7db38d3bb5d376b1aad26accc8aa08567cf
ittehadzihan <zihan991@gmail.com>
2021-11-02 16:08:43 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Zihan';
+
+const welcomeMessage = 'Welcome to Flatbook, Zihan!';
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, ZIHAN!"
+
+const shortGreeting = "Welcome, Z!"
