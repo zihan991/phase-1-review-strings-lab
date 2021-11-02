ba24174a68d79794c5e22eebfc19797020bbcc08
ittehadzihan <zihan991@gmail.com>
2021-11-02 16:40:28 -0400

    Done

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your code in this file!
 const currentUser = 'Zihan';
 
-const welcomeMessage = 'Welcome to Flatbook, Zihan!';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK, ZIHAN!"
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = "Welcome, Z!"
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`
